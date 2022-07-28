5d46211cd136000010da96ca613eb14740a0ecf3
iqbal <iqbal.alam59@gmail.com>
2022-07-28 18:20:46 +0530

    Last 2 days

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1102,6 +1102,7 @@
             ranges: {
                 'Today': [moment(), moment()],
                 'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 2 Days': [moment().subtract(2, 'days'), moment().subtract(1, 'days')],
                 'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                 'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                 'This Month': [moment().startOf('month'), moment().endOf('month')],
