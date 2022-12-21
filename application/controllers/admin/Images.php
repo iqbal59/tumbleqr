14f6517143f3892d14bccc0ffa2debb557407a87
iqbal alam <iqbal.alam59@gmail.com>
2022-12-21 10:46:45 +0530

    GN e removed from img

--- application/controllers/admin/Images.php
+++ application/controllers/admin/Images.php
@@ -1,11 +1,11 @@
 <?php
+
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
 class Images extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
@@ -16,118 +16,119 @@ class Images extends CI_Controller
 
 
  public function viewgallery($order_no, $store_id)
-    {
-        $data['order_no']=$order_no;
-        $data['storeData']=$this->store_model->get_store_by_id($store_id);
-        $data['pictures']=$this->common_model->getPictures($order_no, $store_id);
-        $this->load->view('admin/picture/gallery.php', $data);
-
-        
-
-    }
+ {
+     $data['order_no']=$order_no;
+     $data['storeData']=$this->store_model->get_store_by_id($store_id);
+     $data['pictures']=$this->common_model->getPictures($order_no, $store_id);
+     $this->load->view('admin/picture/gallery.php', $data);
+ }
 
 
-public function sendPhotoEmail(){
+public function sendPhotoEmail()
+{
     $data['pictures']=$this->common_model->getPicturestoEmail();
     //$this->send("TEST ORDER NO", "TEST STORE");
-    foreach ($data['pictures'] as $p){
-       $this->send($p['Order_No'], $p['store_id'], $p['Barcode']);
+    foreach ($data['pictures'] as $p) {
+        $this->send($p['Order_No'], $p['store_id'], $p['Barcode']);
         $this->common_model->update_email_status($p['Order_No'], $p['store_id']);
-       
     }
-    
 }
 
-function getCustomerEmail($barcode=''){
+public function getCustomerEmail($barcode='')
+{
     $data = array(
     'Barcode' => $barcode,
     'StoreCode' => 'TS0',
     'Token' => 'QhI3bD3cR7iF8kL6wW5DC'
 );
 
-$url = "https://api.quickdrycleaning.com/APIQDC/CustomerInfo";
-$postdata = json_encode($data);
-$ch = curl_init($url); 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-$result = curl_exec($ch);
-curl_close($ch);
-$info=json_decode($result);
-if($info)
-return $info->EmailID; 
-else
-return;
+    $url = "https://api.quickdrycleaning.com/APIQDC/CustomerInfo";
+    $postdata = json_encode($data);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $info=json_decode($result);
+    if ($info) {
+        return $info->EmailID;
+    } else {
+        return;
+    }
 }
 
 
 
 
  public function send($order_no, $store_id, $barcode)
-    {
-        // Load PHPMailer library
-        $this->load->library('PHPMailer_Lib');
+ {
+     // Load PHPMailer library
+     $this->load->library('PHPMailer_Lib');
 
-        // PHPMailer object
-        $mail = $this->phpmailer_lib->load();
+     // PHPMailer object
+     $mail = $this->phpmailer_lib->load();
 
-        // SMTP configuration
-        $mail->isSMTP();
-        $mail->Host = 'smtp.office365.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'order_update@tumbledry.in';
-        $mail->Password = 'Fuw39630';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-
-        // $mail->Host = 'mail.centuryfasteners.in';
-        // $mail->SMTPAuth = true;
-        // $mail->Username = 'admin@centuryfasteners.in';
-        // $mail->Password = 'B5]DIG&#OcNH';
-        // $mail->SMTPSecure = 'ssl';
-        // $mail->Port = 465;
-
-        $mail->setFrom('order_update@tumbledry.in', 'Tumbledry - Order Update');
-        $mail->addReplyTo('order_update@tumbledry.in', 'Tumbledry - Order Update');
-
-        
-        // Add a recipient
-//GET EMAIL ID OF CUSTOMERS
-        $customer_email=$this->getCustomerEmail($barcode);
-          if($customer_email)  
-          {$mail->addAddress($customer_email);}
-          else { return;} 
-
-$storeData=$this->common_model->get_store_email($store_id, "tbl_store");
-$store_email=$storeData->email_id;
-if($store_email) 
-$mail->addBCC($store_email);
-
-//END EMAIL ID OF CUSTOMERS
-
-        $mail->addBCC('Gaurav.Nigam@tumbledry.in');
-        //$mail->addAddress('iqbal.alam59@gmail.com');
-
-        // Add cc or bcc
-       // $mail->addCC('Gaurav.Teotia@tumbledry.in');
-        $mail->addBCC('gaurishankarm@gmail.com');
-        $mail->addBCC('kunal.batra@tumbledry.in');
-       // $mail->addCC('tumbledryfactory@gmail.com');
-        //$mail->addBCC('iqbal.alam59@gmail.com');
+     // SMTP configuration
+     $mail->isSMTP();
+     $mail->Host = 'smtp.office365.com';
+     $mail->SMTPAuth = true;
+     $mail->Username = 'order_update@tumbledry.in';
+     $mail->Password = 'Fuw39630';
+     $mail->SMTPSecure = 'tls';
+     $mail->Port = 587;
 
 
-        // Email subject
-        $mail->Subject = "Update: Tumbledry Order # ".$order_no;
+     // $mail->Host = 'mail.centuryfasteners.in';
+     // $mail->SMTPAuth = true;
+     // $mail->Username = 'admin@centuryfasteners.in';
+     // $mail->Password = 'B5]DIG&#OcNH';
+     // $mail->SMTPSecure = 'ssl';
+     // $mail->Port = 465;
 
-        // Set email format to HTML
-        $mail->isHTML(true);
+     $mail->setFrom('order_update@tumbledry.in', 'Tumbledry - Order Update');
+     $mail->addReplyTo('order_update@tumbledry.in', 'Tumbledry - Order Update');
 
-        // Email body content
-        $url="https://centuryfasteners.in/tumbleqr/admin/images/viewgallery/".$order_no."/".$store_id;
-        
-        $mailContent = "Dear Customer<br><br>
+
+     // Add a recipient
+     //GET EMAIL ID OF CUSTOMERS
+     $customer_email=$this->getCustomerEmail($barcode);
+     if ($customer_email) {
+         $mail->addAddress($customer_email);
+     } else {
+         return;
+     }
+
+     $storeData=$this->common_model->get_store_email($store_id, "tbl_store");
+     $store_email=$storeData->email_id;
+     if ($store_email) {
+         $mail->addBCC($store_email);
+     }
+
+     //END EMAIL ID OF CUSTOMERS
+
+     //$mail->addBCC('Gaurav.Nigam@tumbledry.in');
+     //$mail->addAddress('iqbal.alam59@gmail.com');
+
+     // Add cc or bcc
+     // $mail->addCC('Gaurav.Teotia@tumbledry.in');
+     //$mail->addBCC('gaurishankarm@gmail.com');
+     $mail->addBCC('kunal.batra@tumbledry.in');
+     // $mail->addCC('tumbledryfactory@gmail.com');
+     //$mail->addBCC('iqbal.alam59@gmail.com');
+
+
+     // Email subject
+     $mail->Subject = "Update: Tumbledry Order # ".$order_no;
+
+     // Set email format to HTML
+     $mail->isHTML(true);
+
+     // Email body content
+     $url="https://centuryfasteners.in/tumbleqr/admin/images/viewgallery/".$order_no."/".$store_id;
+
+     $mailContent = "Dear Customer<br><br>
  
 Thank you for choosing Tumbledry as your Dry Clean Service Partner. This is an update regarding your order # ".$order_no.".<br><br>
  
@@ -142,16 +143,14 @@ Warm Regards,<br>
 Tumbledry <br><br><br><br><br> <em>This is an auto generated mail. Please do not reply to this. To know more about our services, please visit: <a href='https://www.tumbledry.in'>www.tumbledry.in</a>
 </em>";
 
-        $mail->Body = $mailContent;
+     $mail->Body = $mailContent;
 
-        // Send email
-        if (!$mail->send()) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo 'Message has been sent';
-        }
-    }
-
-
+     // Send email
+     if (!$mail->send()) {
+         echo 'Message could not be sent.';
+         echo 'Mailer Error: ' . $mail->ErrorInfo;
+     } else {
+         echo 'Message has been sent';
+     }
+ }
 }
