724677d538a86a36f1fa0953779339d3369ecd9a
iqbal <iqbal.alam59@gmail.com>
2024-05-02 16:02:42 +0530

    update email

--- application/views/admin/mail/mailpicture.php
+++ application/views/admin/mail/mailpicture.php
@@ -21,36 +21,36 @@
                 if (!$image['picture_new'])
                     continue;
                 ?>
-            <tr>
-                <td align="center" style="padding-top:15px;">
+                <tr>
+                    <td align="center" style="padding-top:15px;">
 
-                    <div
-                        style="width:564px; height:<?php echo $i == 0 ? '372px' : '332px' ?>'; background:url('https://simplifytumbledry.in/assets/emailer/before_after/<?php echo $i == 0 ? 'first_row_bg.png' : 'bg-image.png' ?>')  no-repeat bottom center; margin :0 auto; ">
-                        <div style="<?php echo $i == 0 ? 'padding:50px' : 'padding:0px 50px 50px 50px' ?>">
-                            <table align="center" border:"0" width="98%">
+                        <div
+                            style="width:564px; height:<?php echo $i == 0 ? '372px' : '332px' ?>'; background:url('https://simplifytumbledry.in/assets/emailer/before_after/<?php echo $i == 0 ? 'first_row_bg.png' : 'bg-image.png' ?>')  no-repeat bottom center; margin :0 auto;">
+                            <div style="<?php echo $i == 0 ? 'padding:50px' : 'padding:0px 50px 50px 50px' ?>">
+                                <table align="center" border:"0" width="98%">
 
-                                <tbody>
-                                    <tr>
-                                        <td align="left" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
+                                    <tbody>
+                                        <tr>
+                                            <td align="left" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
 
-                                            <img width="217" height="270"
-                                                src="https://swatinfosystems.com/upload/<?php echo $image['picture']; ?>"
-                                                alt="" style="object-fit: contain;">
-                                        </td>
-                                        <td align="right" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
-                                            <img width="217" height="270" style="object-fit: contain;"
-                                                src="https://swatinfosystems.com/upload/<?php echo $image['picture_new']; ?>"
-                                                alt="">
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                                                <img width="217" height="270"
+                                                    src="https://swatinfosystems.com/upload/<?php echo $image['picture']; ?>"
+                                                    alt="" style="object-fit: contain;">
+                                            </td>
+                                            <td align="right" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
+                                                <img width="217" height="270" style="object-fit: contain;"
+                                                    src="https://swatinfosystems.com/upload/<?php echo $image['picture_new']; ?>"
+                                                    alt="">
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
-                </td>
-            </tr>
+                    </td>
+                </tr>
 
-            <?php $i++;
+                <?php $i++;
             } ?>
 
 
