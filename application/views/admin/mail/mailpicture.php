2a3d915f3deae8f395f8631c0ec5f5a42a964b82
iqbal <iqbal.alam59@gmail.com>
2024-05-02 16:35:13 +0530

    update email

--- application/views/admin/mail/mailpicture.php
+++ application/views/admin/mail/mailpicture.php
@@ -15,42 +15,42 @@
 
             <?php
             $i = 0;
-            //$imagesData = array(array('picture' => '6633314e61358.jpg', 'picture_new' => '6633314e61358.jpg'), array('picture' => '6633314e61358.jpg', 'picture_new' => '6633314e61358.jpg'));
+            $imagesData = array(array('picture' => '6633314e61358.jpg', 'picture_new' => '6633314e61358.jpg'), array('picture' => '6633314e61358.jpg', 'picture_new' => '6633314e61358.jpg'));
             foreach ($imagesData as $image) {
 
                 if (!$image['picture_new'])
                     continue;
                 ?>
-            <tr>
-                <td align="center">
+                <tr>
+                    <td align="center">
 
-                    <div
-                        style="width:564px; height:<?php echo $i == 0 ? '372px' : '332px' ?>'; background:url('https://simplifytumbledry.in/assets/emailer/before_after/<?php echo $i == 0 ? 'first_row_bg.png' : 'bg-image.png' ?>')  no-repeat bottom center; margin :0 auto;">
-                        <div style="<?php echo $i == 0 ? 'padding:50px' : 'padding:0px 50px 50px 50px' ?>">
-                            <table align="center" border:"0" width="98%">
+                        <div
+                            style="width:564px; height:<?php echo $i == 0 ? '372px' : '332px' ?>; background:url('https://simplifytumbledry.in/assets/emailer/before_after/<?php echo $i == 0 ? 'first_row_bg.png' : 'bg-image.png' ?>')  no-repeat bottom center; margin :0 auto;">
+                            <div style="<?php echo $i == 0 ? 'padding:50px' : 'padding:0px 50px 50px 50px' ?>">
+                                <table align="center" border:"0" width="98%">
 
-                                <tbody>
-                                    <tr>
-                                        <td align="left" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
+                                    <tbody>
+                                        <tr>
+                                            <td align="left" style="<?php echo $i == 0 ? '' : 'padding-top:13px;' ?>">
 
-                                            <img width="217" height="270"
-                                                src="https://swatinfosystems.com/upload/<?php echo $image['picture']; ?>"
-                                                alt="" style="object-fit: contain;">
-                                        </td>
-                                        <td align="right" style="<?php echo $i == 0 ? '' : 'padding-top:15px;' ?>">
-                                            <img width="217" height="270" style="object-fit: contain;"
-                                                src="https://swatinfosystems.com/upload/<?php echo $image['picture_new']; ?>"
-                                                alt="">
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                                                <img width="217" height="270"
+                                                    src="https://swatinfosystems.com/upload/<?php echo $image['picture']; ?>"
+                                                    alt="" style="object-fit: contain;">
+                                            </td>
+                                            <td align="right" style="<?php echo $i == 0 ? '' : 'padding-top:13px;' ?>">
+                                                <img width="217" height="270" style="object-fit: contain;"
+                                                    src="https://swatinfosystems.com/upload/<?php echo $image['picture_new']; ?>"
+                                                    alt="">
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
-                </td>
-            </tr>
+                    </td>
+                </tr>
 
-            <?php $i++;
+                <?php $i++;
             } ?>
 
 
